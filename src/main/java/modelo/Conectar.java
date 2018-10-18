b3c3e68ae6c45d440b90f33a67fe9b03c35b78c6
Your Name <caleuro51@ucatolica.edu.co>
2018-10-17 20:08:26 -0500

    Compilacion monitorias en maven

--- src/main/java/modelo/Conectar.java
+++ src/main/java/modelo/Conectar.java
@@ -1,6 +1,10 @@
 
 package modelo;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
@@ -11,10 +15,11 @@ public class Conectar {
     public DriverManagerDataSource conectar(){
         DriverManagerDataSource datasource = new DriverManagerDataSource();
         datasource.setDriverClassName("com.mysql.jdbc.Driver");
-        datasource.setUrl("jdbc:mysql://localhost/monitorias");
-        datasource.setUsername("root");
-        datasource.setPassword("root");
+        datasource.setUrl("jdbc:mysql://sql10.freemysqlhosting.net/sql10261706");
+        datasource.setUsername("sql10261706");
+        datasource.setPassword("6VBuTYR4hC");
         
         return datasource;
     }
 }
+
